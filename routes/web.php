f73065a3ea914b004ab5fce6298f074d51a13675
khamal45 <khamal45@github.com>
2025-06-27 17:36:32 +0700

    Add route definitions for House and Tagihan management

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HouseController;
+use App\Http\Controllers\TagihanController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,7 +13,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::resource('houses', HouseController::class);
+    Route::get('/house-management', [HouseController::class, 'management'])->name('houses.management');
+    Route::get('tagihan/{house_resident_id}', [TagihanController::class, 'index']);
+    Route::post('/tagihan/{house_resident_id}/bayar', [TagihanController::class, 'bayar']);
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
