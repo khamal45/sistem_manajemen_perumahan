2e355da1dac96497613c6e3752825160239e5873
khamal45 <khamal45@github.com>
2025-06-29 21:04:16 +0700

    feat: update routing for home and dashboard to redirect to keuangan.index; fix route path for house residents

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ExportExcelController;
+use App\Http\Controllers\FeeTypeController;
 use App\Http\Controllers\FixedExpenseController;
 use App\Http\Controllers\HouseController;
 use App\Http\Controllers\HouseResidentController;
@@ -8,17 +9,16 @@ use App\Http\Controllers\KeuanganController;
 use App\Http\Controllers\PengeluaranController;
 use App\Http\Controllers\ResidentController;
 use App\Http\Controllers\TagihanController;
-use App\Models\HouseResidents;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return redirect()->route('keuangan.index');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
+        return redirect()->route('keuangan.index');
     })->name('dashboard');
     Route::resource('houses', HouseController::class);
     Route::get('/house-management', [HouseController::class, 'management'])->name('houses.management');
@@ -27,7 +27,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('house-resident', HouseResidentController::class)->except(['create']);
     Route::get('house-resident/{house_id}/create', [HouseResidentController::class, 'create']);
 
-    Route::get('/houses/{house}/residents', [HouseResidentController::class, 'showByHouse'])->name('house_resident.by_house');
+    Route::get('/house-resident/{house}/residents', [HouseResidentController::class, 'showByHouse'])->name('house_resident.by_house');
 
     //resident
     Route::get('/resident/{house_residents_id}/create', [ResidentController::class, 'create']);
@@ -51,6 +51,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/pengeluaran-tetap/bayar', [FixedExpenseController::class, 'bayarUnpaid'])->name('pengeluaran.bayar_unpaid');
     Route::resource('/fixed-expense', FixedExpenseController::class);
     Route::get('/export-keuangan', [ExportExcelController::class, 'export']);
+
+    Route::resource('/pemasukan', FeeTypeController::class);
 });
 
 require __DIR__ . '/settings.php';
